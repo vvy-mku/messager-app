0e28e74b035a33e1be64e49f4a4885325de1ec2b
vvy-mku <vvy.mku@gmail.com>
2024-04-26 15:26:36 +0300

    Login and registration MUI forms

--- messager-app/src/Login.js
+++ messager-app/src/Login.js
@@ -1,5 +1,115 @@
+import {
+  Card,
+  Stack,
+  TextField,
+  Button,
+  CardHeader,
+  CardContent,
+  Link,
+  Alert,
+} from "@mui/material";
+
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 const Login = () => {
-  return <h1>Login form</h1>;
+  const [state, setState] = useState({
+    login: "",
+    password: "",
+    invalid: false,
+  });
+  const navigate = useNavigate();
+
+  console.log("rerender");
+
+  const onLogin = () => {
+    if (state.login.length == 0 || state.password.length == 0) {
+      setState({ ...state, invalid: true });
+      return;
+    }
+
+    const data = {
+      login: state.login,
+      password: state.password,
+    };
+
+    console.log(state);
+
+    fetch("/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (response.status == 200) {
+          navigate("/account");
+        } else {
+          setState({ ...state, invalid: true });
+        }
+      })
+      .catch(() => {});
+  };
+
+  return (
+    <Stack justifyContent="center" alignItems="center" sx={{ height: "100vh" }}>
+      <Card sx={{ width: "30vw" }}>
+        <CardHeader title="Login" />
+        <CardContent>
+          <Stack spacing={2}>
+            {state.invalid ? (
+              <Alert severity="warning">Invalid login or password.</Alert>
+            ) : (
+              ""
+            )}
+            <TextField
+              hiddenLabel
+              id="login"
+              defaultValue=""
+              label="Logim"
+              value={state.login}
+              onChange={(event) => {
+                setState({
+                  ...state,
+                  login: event.target.value,
+                  invalid: false,
+                });
+              }}
+            />
+            <TextField
+              hiddenLabel
+              type="password"
+              id="password"
+              defaultValue=""
+              label="Password"
+              value={state.password}
+              onChange={(event) => {
+                setState({
+                  ...state,
+                  password: event.target.value,
+                  invalid: false,
+                });
+              }}
+            />
+
+            <Button variant="contained" size="large" onClick={onLogin}>
+              Login
+            </Button>
+            <Link
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/registration");
+              }}
+              href="#"
+            >
+              Create account
+            </Link>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Stack>
+  );
 };
 
 export default Login;
--- messager-app/src/Registration.js
+++ messager-app/src/Registration.js
@@ -1,5 +1,164 @@
+import {
+  Card,
+  Stack,
+  TextField,
+  Button,
+  CardHeader,
+  CardContent,
+  Alert,
+  Link,
+} from "@mui/material";
+
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 const Registration = () => {
-  return <h1>Registration form</h1>;
+  const [state, setState] = useState({
+    login: "",
+    name: "",
+    password: "",
+    confirmPassword: "",
+    error: "",
+  });
+
+  const navigate = useNavigate();
+
+  const onRegistration = () => {
+    if (state.password != state.confirmPassword) {
+      setState({ ...state, error: "diff-passwords" });
+      return;
+    }
+
+    const data = {
+      login: state.login,
+      password: state.password,
+      name: state.name,
+    };
+
+    fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.ok) {
+          navigate("/account");
+        } else {
+          setState({ ...state, error: data.error });
+        }
+      });
+  };
+
+  return (
+    <Stack justifyContent="center" alignItems="center" sx={{ height: "100vh" }}>
+      <Card sx={{ width: "40vw" }}>
+        <CardHeader title="Registration" />
+        <CardContent>
+          <Stack spacing={2}>
+            {state.error == "diff-passwords" ? (
+              <Alert severity="warning">Invalid password confirmation.</Alert>
+            ) : (
+              ""
+            )}
+            {state.error == "bad-name-param" ? (
+              <Alert severity="warning">Invalid name.</Alert>
+            ) : (
+              ""
+            )}
+            {state.error == "bad-login-param" ? (
+              <Alert severity="warning">Invalid Login.</Alert>
+            ) : (
+              ""
+            )}
+            {state.error == "bad-password-param" ? (
+              <Alert severity="warning">Invalid password.</Alert>
+            ) : (
+              ""
+            )}
+            {state.error == "login-is-not-available" ? (
+              <Alert severity="warning">Login you typed already exists.</Alert>
+            ) : (
+              ""
+            )}
+            <TextField
+              hiddenLabel
+              id="login"
+              defaultValue=""
+              label="Login"
+              value={state.login}
+              onChange={(event) => {
+                setState({ ...state, login: event.target.value, error: "" });
+              }}
+              error={
+                state.error == "bad-login-param" ||
+                state.error == "login-is-not-available"
+              }
+            />
+            <TextField
+              hiddenLabel
+              id="name"
+              defaultValue=""
+              label="Name"
+              value={state.name}
+              onChange={(event) => {
+                setState({ ...state, name: event.target.value, error: "" });
+              }}
+              error={state.error == "bad-name-param"}
+            />
+            <TextField
+              hiddenLabel
+              type="password"
+              id="password"
+              defaultValue=""
+              label="Password"
+              value={state.password}
+              onChange={(event) => {
+                setState({ ...state, password: event.target.value, error: "" });
+              }}
+              error={
+                state.error == "bad-password-param" ||
+                state.error == "diff-passwords"
+              }
+            />
+            <TextField
+              hiddenLabel
+              type="password"
+              id="password_confirm"
+              defaultValue=""
+              label="Confirm password"
+              value={state.confirmPassword}
+              onChange={(event) => {
+                setState({
+                  ...state,
+                  confirmPassword: event.target.value,
+                  error: "",
+                });
+              }}
+              error={
+                state.error == "bad-password-param" ||
+                state.error == "diff-passwords"
+              }
+            />
+            <Button variant="contained" size="large" onClick={onRegistration}>
+              Registration
+            </Button>
+            <Link
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/login");
+              }}
+              href="#"
+            >
+              Already have an account
+            </Link>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Stack>
+  );
 };
 
 export default Registration;
